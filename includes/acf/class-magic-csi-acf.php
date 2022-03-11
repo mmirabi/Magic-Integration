6ce3799c7125e7de3676cf88a621585b08bc7877
Mehdi Mirabi <mehdi.mirabi@gmail.com>
2022-03-11 14:54:12 +0300

    ACF json added

---
+++ includes/acf/class-magic-csi-acf.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace magic_CSI;
+
+/**
+* Class admin 
+*/
+
+class ACF {
+    /**
+     *  ACF constructor
+     */
+    public function __construct() {
+
+        add_filter( 'acf/settings/load_json', [$this, 'loadAcfJson'] );
+
+        add_filter( 'aljm_save_json', function ( $folders ) {
+            $folders['Magic'] = MAGIC_CSI_ABSPATH . 'includes/acf/local-json';
+
+            return $folders;load<?php
+
+            namespace Magic_CSI;
+            
+            /**
+             * Class Admin
+             * @package Magic_CSI
+             */
+            class ACF {
+                /**
+                 * ACF constructor.
+                 */
+                public function __construct() {
+            
+                    add_filter( 'acf/settings/load_json', [ $this, 'loadAcfJson' ] );
+            
+            
+                    add_filter( 'aljm_save_json', function ( $folders ) {
+                        $folders['Magic'] = MAGIC_CSI_ABSPATH . 'includes/acf/local-json';
+            
+                        return $folders;
+                    } );
+                }
+            
+                /**
+                 * Load saved ACF config
+                 *
+                 * @param $paths
+                 *
+                 * @return mixed
+                 */
+                function loadAcfJson( $paths ) {
+            
+                    // append path
+                    $paths[] = MAGIC_CSI_ABSPATH . 'includes/acf/local-json';
+            
+                    return $paths;
+                }
+            
+            }
+            
+            new ACF();
+            
+        } );
+    }
+
+    /**
+     *  Load saved ACF config
+     * 
+     * @param $paths
+     * 
+     * @return mixed
+     */
+    function loadAcfJson( $paths ) {
+        
+        // append path
+        $paths[] = MAGIC_CSI_ABSPATH . 'includs/acf/local-json';
+
+        return $paths;
+    }
+}
+
+new ACF();
--- includes/class-magic-csi.php
+++ includes/class-magic-csi.php
@@ -1,18 +1,69 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
+	exit;
+} // Exit if accessed directly
 
 class Magic_CSI {
+	/**
+	 * Magic_CSI constructor.
+	 */
+	public function __construct() {
+		$this->set_constants();
+		$this->includes();
+		$this->init_hooks();
+	}
 
-    public function __construct() {
-        $this->set_construct();
-        $this->includes();
-        $this->init_hooks();
-    }
+	/**
+	 * Define Constants.
+	 */
+	private function set_constants() {
+		define( 'MAGIC_CSI_ABSPATH', plugin_dir_path( MAGIC_CSI_PLUGIN_FILE ) );
+		define( 'MAGIC_CSI_URL', plugin_dir_url( dirname( __FILE__ ) ) );
+	}
 
-    private function set_constants() {
-        define( 'MAGIC_CSI_ABSPATH', plugin_dir_path( MAGIC_CSI_PLUGIN_FINE )
-    }
+	/**
+	 * Initial plugin setup.
+	 */
+	private function init_hooks() {
+
+		// Load text domain
+		add_action( 'init', array( $this, 'load_textdomain' ) );
+	}
+
+	/**
+	 * Load plugin textdomain.
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'magic-csi', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+
+	/**
+	 * Includes classes and functions.
+	 */
+	public function includes() {
+//		require_once MAGIC_CSI_ABSPATH . 'includes/class-magic-csi-install.php';
+
+		/*		if ( is_admin() ) {
+					require_once MAGIC_CSI_ABSPATH . 'includes/admin/class-magic-csi-admin.php';
+					require_once MAGIC_CSI_ABSPATH . 'includes/admin/class-magic-csi-settings.php';
+				} else {
+					require_once MAGIC_CSI_ABSPATH . 'includes/class-magic-csi-public.php';
+				}*/
+
+		// Utility classes.
+		require_once MAGIC_CSI_ABSPATH . 'includes/acf/class-magic-csi-acf.php';
+		require_once MAGIC_CSI_ABSPATH . 'includes/class-magic-csi-public.php';
+		require_once MAGIC_CSI_ABSPATH . 'includes/woocommerce/class-magic-csi-woocommerce.php';
+
+		// Load lumise class.
+		require_once MAGIC_CSI_ABSPATH . 'includes/lumise/class-magic-lumise.php';
+
+		/*// API classes.
+		require_once MAGIC_CSI_ABSPATH . 'includes/class-magic-csi-rest-api.php';
+		require_once MAGIC_CSI_ABSPATH . 'includes/api/v1/class-magic-csi-api-controller.php';*/
+
+		// Template functions.
+		require_once MAGIC_CSI_ABSPATH . 'includes/template-functions.php';
+	}
 }
